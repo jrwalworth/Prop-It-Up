c07ff4264d8605df2a87152e34cb36cab30f92ba
Jason Walworth <jrwalworth@gmail.com>
2022-04-08 11:41:44 -0700

    Added PersonCard for React props assignment.

---
+++ src/components/PersonCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor} = props;
+    return (
+        <div className="Person">
+            <h2>{lastName}, {firstName}</h2>
+            <div>
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+            </div>
+        </div>
+    )
+    
+}
+
+export default PersonCard;
